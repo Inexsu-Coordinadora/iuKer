8ef50bde74c1c8c913b4ca33208604060b605691
isrameve <israelmedive@gmail.com>
2025-11-01 17:40:48 -0500

    feat: Implementada la clase que cumple con IPacientes para poderla instanciar desde casosDeUso

---
+++ src/core/dominio/Paciente/Paciente.ts
@@ -0,0 +1,22 @@
+import { IPaciente } from './IPaciente.js';
+
+export class Paciente implements IPaciente {
+  public idPaciente?: string;
+  public nombre: string;
+  public apellidos: string;
+  public fecha_nacimiento: Date;
+  public sexo: string;
+  public email: string;
+  public telefono: string;
+  public direccion: string;
+
+  constructor(data: IPaciente) {
+    this.nombre = data.nombre;
+    this.apellidos = data.apellidos;
+    this.fecha_nacimiento = data.fecha_nacimiento;
+    this.sexo = data.sexo;
+    this.email = data.email;
+    this.telefono = data.telefono;
+    this.direccion = data.direccion;
+  }
+}
