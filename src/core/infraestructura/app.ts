4da46bd1894d491f94b7535f15630730accf8397
Samuel Alvarez <samuel.alvarezbecerra.57@gmail.com>
2025-11-02 18:03:38 -0500

    feat: integrar enrutador de consultorio

--- src/core/infraestructura/app.ts
+++ src/core/infraestructura/app.ts
@@ -1,14 +1,14 @@
 import Fastify from "fastify";
 import { FastifyError } from "fastify";
 import { configuracion } from "../../common/configuracion.js";
+import { construirConsultorioEnrutador } from "./rutas/consultoriosEnrutador.js";
 // Se debe agregar enrutador
 
 const app = Fastify({ logger: true });
 
 app.register(
   async (appInstance) => {
-    // Se debe inicializar el enrutador
-    // construirPlatosEnrutador(appInstance);
+    construirConsultorioEnrutador(appInstance)
   },
   { prefix: "/api" }
 );
@@ -16,9 +16,9 @@ app.register(
 export const startServer = async (): Promise<void> => {
   try {
     await app.listen({ port: configuracion.httpPuerto });
-    app.log.info("✅ El servidor esta corriendo...");
+    app.log.info("El servidor esta corriendo...");
   } catch (err) {
-    app.log.error(`❌ Error al ejecutar el servidor\n ${err}`);
+    app.log.error(`Error al ejecutar el servidor\n ${err}`);
     const serverError: FastifyError = {
       code: "FST_ERR_INIT_SERVER",
       name: "ServidorError",
