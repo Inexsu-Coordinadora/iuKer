668b459ffc4a85a435d330eb10297aa299f496c0
Samuel Alvarez <samuel.alvarezbecerra.57@gmail.com>
2025-11-02 17:56:18 -0500

    refactor: Cambiar retorno de eliminarConsultorio de boolean a void

--- src/core/dominio/Consultorio/IRepositorioConsultorio.ts
+++ src/core/dominio/Consultorio/IRepositorioConsultorio.ts
@@ -2,9 +2,9 @@ import { IConsultorio } from "./IConsultorio.js";
 // import { Pool } from "pg";
 
 export interface IRepositorioConsultorio {
-  agregarConsultorios(datosConsultorio: IConsultorio): Promise<string>;
-  listarConsultorio(limite?: number): Promise<IConsultorio[]>;
+  agregarConsultorio(datosConsultorio: IConsultorio): Promise<string>;
+  listarConsultorios(limite?: number): Promise<IConsultorio[]>;
   obtenerConsultorioPorId(idConsultorio: string): Promise<IConsultorio | null>;
   actualizarConsultorio(idConsultorio: string, datosConsultorio: IConsultorio): Promise<IConsultorio | null>;
-  eliminarConsultorio(idConsultorio: string): Promise<boolean>;
+  eliminarConsultorio(idConsultorio: string): Promise<void>;
 }
