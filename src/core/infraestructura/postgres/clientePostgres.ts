c4c8d5360077dae833f081c66e965f0f0a544f66
Edwin Peña <edwinsp095@gmail.com>
2025-11-03 19:42:24 -0500

    fix: agregar tipo Date al cliente de prostgres que ejecuta la consulta

--- src/core/infraestructura/postgres/clientePostgres.ts
+++ src/core/infraestructura/postgres/clientePostgres.ts
@@ -1,5 +1,5 @@
-import { Pool } from "pg";
-import { configuracion } from "../../../common/Configuracion.js";
+import { Pool } from 'pg';
+import { configuracion } from '../../../common/Configuracion.js';
 
 const pool = new Pool({
   host: configuracion.baseDatos.host,
@@ -9,9 +9,6 @@ const pool = new Pool({
   password: configuracion.baseDatos.contrasena,
 });
 
-export async function ejecutarConsulta(
-  consulta: string,
-  parametros?: Array<number | string>
-) {
+export async function ejecutarConsulta(consulta: string, parametros?: Array<number | string | Date>) {
   return await pool.query(consulta, parametros);
 }
