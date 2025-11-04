adb2e0114e3f37bf796c8ecdb8fb1080b1b12698
Edwin Peña <edwinsp095@gmail.com>
2025-11-03 19:13:46 -0500

    feat: crear esquema para validar datos recibidos

---
+++ src/core/infraestructura/esquemas/citaMedicaEsquema.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+
+export const crearCitaMedicaEsquema = z.object({
+  medico: z
+    .string()
+    .nonempty('El médico debe tener identificador profesional')
+    .min(5, 'El documento profesional del médico debe tener mínimo 6 carateres')
+    .max(15, 'El documento profesional del médico debe tener máximo 15 caráteres')
+    .regex(/^[A-Za-z0-9]+$/, 'El ID del médico solo debe contener letras y números'),
+
+  tipoDocPaciente: z
+    .number()
+    .int('El documento de identidad debe ser un etero')
+    .nonnegative('El documento de identidad debe ser un número positivo')
+    .max(1, 'El documento de identidad debe tener máximo 15 caráteres'),
+
+  numeroDocPaciente: z
+    .string()
+    .nonempty('El paciente debe tener un numero de identificación')
+    .min(6, 'El documento de identidad del paciente debe tener mínimo 6 carateres')
+    .max(15, 'El documento de identidad del paciente debe tener máximo 15 carateres')
+    .regex(/^[A-Za-z0-9]+$/, 'El ID del médico solo debe contener letras y números'),
+
+  idConsultorio: z
+    .string()
+    .nonempty('Se sebe seleccionar el consultorio')
+    .length(4, ' El id del consultorio debe ser de 4 carateres')
+    .regex(/^[A-Za-z0-9]+$/, 'El consultorio solo debe contener letras y números'),
+
+  fecha: z.coerce.date().refine((fecha) => fecha > new Date(), {
+    message: 'La fecha de la cita debe ser futura',
+  }),
+
+  horaInicio: z
+    .string()
+    .nonempty('La hora de inicio es obligatoria')
+    .regex(/^[0-9:]+$/, 'La hora debe estar en formato HH:MM (ej. 14:00)')
+    .length(5, 'El formato de hora debe ser exactamente HH:MM (5 caracteres)'),
+
+  duracion: z
+    .string()
+    .nonempty('Debes especificar la duración como "X minutos"')
+    .min(9, 'La duración debe tener mínimo 9 carateres')
+    .max(11, 'La duración debe tener máximo 11 carateres')
+    .regex(/^[A-Za-z0-9\s]+$/, 'La duración debe contener letras y números'),
+});
+
+export type citaMedicaDTO = z.infer<typeof crearCitaMedicaEsquema>;
