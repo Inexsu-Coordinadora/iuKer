04b9c834ba67d115553adcc01c1141c1016633a3
Sebastián Rave - Personal <juansebastianrm123@gmail.com>
2025-11-04 00:51:12 -0500

    refactor: Adaptación del método actualizarMedico en IMedicoRepositorio.ts para soportar campos parciales

--- src/core/dominio/Medico/IMedicoRepositorio.ts
+++ src/core/dominio/Medico/IMedicoRepositorio.ts
@@ -4,6 +4,6 @@ export interface IMedicoRepositorio {
     crearMedico(datosMedico : IMedico) : Promise <string>;
     listarMedicos(limite? : number) : Promise <IMedico[]>;
     obtenerMedicoPorTarjetaProfesional(tarjetaProfesional : string) : Promise <IMedico | null>; 
-    actualizarMedico(tarjetaProfesional : string, datosMedico : IMedico) : Promise <IMedico | null>;
+    actualizarMedico(tarjetaProfesional : string, datosMedico : Partial <IMedico>) : Promise <IMedico | null>;
     eliminarMedico(tarjetaProfesional : string) : Promise <void>;
 };
