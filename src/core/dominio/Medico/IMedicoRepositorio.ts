2d308dcb2cd5b098d3527d609f027a039b38c06b
Sebastián Rave - Personal <juansebastianrm123@gmail.com>
2025-11-04 00:36:36 -0500

    refactor: Permitir un retorno null para obtenerMedicoPorTarjetaProfesional en IMedicoRepositorio.ts

--- src/core/dominio/Medico/IMedicoRepositorio.ts
+++ src/core/dominio/Medico/IMedicoRepositorio.ts
@@ -3,7 +3,7 @@ import { IMedico } from './IMedico.js'
 export interface IMedicoRepositorio {
     crearMedico(datosMedico : IMedico) : Promise <string>;
     listarMedicos(limite? : number) : Promise <IMedico[]>;
-    obtenerMedicoPorTarjetaProfesional(tarjetaProfesional : string) : Promise <IMedico>; 
+    obtenerMedicoPorTarjetaProfesional(tarjetaProfesional : string) : Promise <IMedico | null>; 
     actualizarMedico(tarjetaProfesional : string, datosMedico : IMedico) : Promise <IMedico | null>;
     eliminarMedico(tarjetaProfesional : string) : Promise <void>;
 };
