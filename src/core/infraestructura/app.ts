5dc05e938dd87764a3f141aeffeb4eeb8d2f7d18
Edwin Peña <edwinsp095@gmail.com>
2025-11-03 20:03:42 -0500

    chore: integrar el enrutador de citas médicas en la aplicación principal

--- src/core/infraestructura/app.ts
+++ src/core/infraestructura/app.ts
@@ -1,27 +1,27 @@
-import Fastify from "fastify";
-import { FastifyError } from "fastify";
-import { configuracion } from "../../common/Configuracion.js";
+import Fastify from 'fastify';
+import { FastifyError } from 'fastify';
+import { configuracion } from '../../common/Configuracion.js';
+import { construirCitasEnrutados } from './rutas/citasMedicasEnrutador.js';
 // Se debe agregar enrutador
 
 const app = Fastify({ logger: true });
 
 app.register(
   async (appInstance) => {
-    // Se debe inicializar el enrutador
-    // construirPlatosEnrutador(appInstance);
+    construirCitasEnrutados(appInstance);
   },
-  { prefix: "/api" }
+  { prefix: '/api' }
 );
 
 export const startServer = async (): Promise<void> => {
   try {
     await app.listen({ port: configuracion.httpPuerto });
-    app.log.info("✅ El servidor esta corriendo...");
+    app.log.info('✅ El servidor esta corriendo...');
   } catch (err) {
     app.log.error(`❌ Error al ejecutar el servidor\n ${err}`);
     const serverError: FastifyError = {
-      code: "FST_ERR_INIT_SERVER",
-      name: "ServidorError",
+      code: 'FST_ERR_INIT_SERVER',
+      name: 'ServidorError',
       statusCode: 500,
       message: `El servidor no se pudo iniciar: ${(err as Error).message}`,
     };
