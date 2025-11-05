9698479695f28ad894949630546b3b3724e6d1a9
Samuel Alvarez <samuel.alvarezbecerra.57@gmail.com>
2025-11-04 21:18:08 -0500

    refactor: actualizar archivos de migracion BBDD

--- migraciones/pacientes/iuKer_creacion_tablas.sql
+++ migraciones/pacientes/iuKer_creacion_tablas.sql
@@ -1,9 +1,6 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
---SELECT * FROM pg_extension WHERE extname = 'uuid-ossp';
-
-DROP SCHEMA IF EXISTS iuker;
+DROP SCHEMA IF EXISTS iuker CASCADE;
 CREATE SCHEMA iuker;
-SET search_path TO iuker;
+ALTER DATABASE iukerdb SET search_path TO iuker, public;
 
 CREATE TABLE IF NOT EXISTS tipo_documentos(
   id_documento INT PRIMARY KEY,
@@ -16,8 +13,8 @@ CREATE TABLE IF NOT EXISTS estados(
 );
 
 CREATE TABLE IF NOT EXISTS pacientes (
-  numero_doc VARCHAR(15) UNIQUE,
   tipo_doc INT NOT NULL REFERENCES tipo_documentos (id_documento),
+  numero_doc VARCHAR(15) NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
   fecha_nacimiento DATE NOT NULL,
@@ -25,12 +22,13 @@ CREATE TABLE IF NOT EXISTS pacientes (
   email VARCHAR(200) NOT NULL,
   telefono VARCHAR(20) NOT NULL,
   direccion VARCHAR(100) NOT NULL,
-  
-  PRIMARY KEY (numero_doc, tipo_doc)
+
+  PRIMARY KEY (tipo_doc, numero_doc)
 );
 
 CREATE TABLE IF NOT EXISTS medicos (
   tarjeta_profesional VARCHAR(15) PRIMARY KEY,
+  tipo_doc INT NOT NULL REFERENCES tipo_documentos (id_documento),
   numero_doc VARCHAR(15) NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
@@ -48,20 +46,14 @@ CREATE TABLE IF NOT EXISTS consultorios (
 );
 
 CREATE TABLE IF NOT EXISTS citas_medicas (
-  id_cita UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  medico VARCHAR(15) NOT NULL REFERENCES medicos (tarjeta_profesional),
+  id_cita VARCHAR(50) PRIMARY KEY,
+  medico VARCHAR(15) REFERENCES medicos (tarjeta_profesional) ON DELETE SET NULL,
   tipo_doc_paciente INT NOT NULL,
   numero_doc_paciente VARCHAR(15) NOT NULL,
-  consultorio VARCHAR(5) NOT NULL REFERENCES consultorios (id_consultorio),
-  fecha DATE NOT NULL,
-  hora_inicio TIME NOT NULL,
-  duracion INTERVAL NOT NULL,
+  id_consultorio VARCHAR(5) NOT NULL REFERENCES consultorios (id_consultorio),
+  fecha VARCHAR(50) NOT NULL,
+  hora_inicio VARCHAR(50) NOT NULL,
+  duracion VARCHAR(20) NOT NULL,
   estado INT NOT NULL REFERENCES estados(id_estado),
-  id_cita_anterior UUID REFERENCES citas_medicas (id_cita),
-  FOREIGN KEY (numero_doc_paciente, tipo_doc_paciente) REFERENCES pacientes(numero_doc, tipo_doc)
+  FOREIGN KEY (tipo_doc_paciente, numero_doc_paciente) REFERENCES pacientes(tipo_doc, numero_doc)
 );
-
-
-
-
-
--- migraciones/pacientes/iuKer_insercion_datos.sql
+++ migraciones/pacientes/iuKer_insercion_datos.sql
@@ -12,27 +12,28 @@ INSERT INTO tipo_documentos (id_documento, descripcion) VALUES
 -- ESTADOS GENERALES
 INSERT INTO estados (id_estado, descripcion) VALUES
 (1, 'Activa'),
-(2, 'Pendiente'),
-(3, 'Cancelada'),
+(2, 'Actualizada'),
+(3, 'Reprogramada'),
 (4, 'Finalizada'),
-(5, 'Ocupado'),
-(6, 'Disponible');
+(5, 'Cancelada'),
+(6, 'Ocupado'),
+(7, 'Disponible');
 
 -- PACIENTES
-INSERT INTO pacientes (numero_doc, tipo_doc, nombre, apellido, fecha_nacimiento, sexo, email, telefono, direccion)
+INSERT INTO pacientes (tipo_doc, numero_doc, nombre, apellido, fecha_nacimiento, sexo, email, telefono, direccion)
 VALUES
-('100001', 1, 'Juan', 'Pérez', '1995-06-15', 'M', 'juan.perez@mail.com', '3001112233', 'Cra 45 #12-30'),
-('100002', 1, 'Laura', 'Gómez', '1992-04-20', 'F', 'laura.gomez@mail.com', '3012223344', 'Cll 80 #45-10'),
-('100003', 1, 'Andrés', 'Ramírez', '1988-10-05', 'M', 'andres.ramirez@mail.com', '3023334455', 'Av 30 #9-22'),
-('100004', 2, 'María', 'López', '2000-01-12', 'F', 'maria.lopez@mail.com', '3044445566', 'Cll 10 #50-60');
+(1, '100001', 'Juan', 'Pérez', '1995-06-15', 'M', 'juan.perez@mail.com', '3001112233', 'Cra 45 #12-30'),
+(3, '100002', 'Laura', 'Gómez', '1992-04-20', 'F', 'laura.gomez@mail.com', '3012223344', 'Cll 80 #45-10'),
+(2, '100003', 'Andrés', 'Ramírez', '1988-10-05', 'M', 'andres.ramirez@mail.com', '3023334455', 'Av 30 #9-22'),
+(4, '100004', 'María', 'López', '2000-01-12', 'F', 'maria.lopez@mail.com', '3044445566', 'Cll 10 #50-60');
 
 -- MÉDICOS
-INSERT INTO medicos (tarjeta_profesional, numero_doc, nombre, apellido, fecha_nacimiento, sexo, especialidad, email, telefono)
+INSERT INTO medicos (tarjeta_profesional, tipo_doc, numero_doc, nombre, apellido, fecha_nacimiento, sexo, especialidad, email, telefono)
 VALUES
-('MP001', '900001', 'Carlos', 'Rodríguez', '1980-03-12', 'M', 'Medicina General', 'carlos.rodriguez@clinicaiuker.com', '3105556677'),
-('MP002', '900002', 'Sofía', 'Martínez', '1985-11-23', 'F', 'Pediatría', 'sofia.martinez@clinicaiuker.com', '3116667788'),
-('MP003', '900003', 'Julián', 'García', '1978-09-08', 'M', 'Cardiología', 'julian.garcia@clinicaiuker.com', '3127778899'),
-('MP004', '900004', 'Valentina', 'Ruiz', '1990-02-17', 'F', 'Dermatología', 'valentina.ruiz@clinicaiuker.com', '3138889900');
+('MP001', 1, '900001', 'Carlos', 'Rodríguez', '1980-03-12', 'M', 'Medicina General', 'carlos.rodriguez@clinicaiuker.com', '3105556677'),
+('MP002', 1, '900002', 'Sofía', 'Martínez', '1985-11-23', 'F', 'Pediatría', 'sofia.martinez@clinicaiuker.com', '3116667788'),
+('MP003', 1, '900003', 'Julián', 'García', '1978-09-08', 'M', 'Cardiología', 'julian.garcia@clinicaiuker.com', '3127778899'),
+('MP004', 1, '900004', 'Valentina', 'Ruiz', '1990-02-17', 'F', 'Dermatología', 'valentina.ruiz@clinicaiuker.com', '3138889900');
 
 -- CONSULTORIOS
 INSERT INTO consultorios (id_consultorio, ubicacion, estado)
@@ -43,10 +44,9 @@ VALUES
 ('C202', 'Piso 2 - Ala Sur', 6);
 
 -- CITAS MÉDICAS
-INSERT INTO citas_medicas (medico, tipo_doc_paciente, numero_doc_paciente, consultorio, fecha, hora_inicio, duracion, estado, id_cita_anterior)
+INSERT INTO citas_medicas (id_cita, medico, tipo_doc_paciente, numero_doc_paciente, id_consultorio, fecha, hora_inicio, duracion, estado)
 VALUES
-('MP001', 1, '100001', 'C101', '2025-10-30', '08:00', INTERVAL '30 minutes', 2, NULL),
-('MP002', 3, '100002', 'C102', '2025-10-30', '09:00', INTERVAL '45 minutes', 1, NULL),
-('MP003', 2, '100003', 'C202', '2025-10-31', '10:30', INTERVAL '60 minutes', 4, NULL),
-('MP004', 1, '100004', 'C201', '2025-11-01', '14:00', INTERVAL '30 minutes', 3, NULL);
-
+('A12546778931mc','MP001', 1, '100001', 'C101', '2025-11-30', '08:00', '30 minutos', 1),
+('B225dds5545','MP002', 3, '100002', 'C102', '2025-11-27', '09:00', '45 minutos', 1),
+('G654465646df16sdf','MP003', 2, '100003', 'C202', '2025-11-29', '10:30', '60 minutos', 1),
+('Hdf54sf1s31fs84ds5','MP004', 4, '100004', 'C201', '2025-11-01', '14:00', '90 minutos', 1);
