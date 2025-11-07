7afc7ec9a530e0026b618a5b4c8608f6b1d3efd3
isrameve <israelmedive@gmail.com>
2025-11-07 09:39:49 -0500

    feature: Avazando en la rama test para ver la implementación de ciertas comprobaciónes del servicio

--- src/core/dominio/Paciente/IRepositorioPacientes.ts
+++ src/core/dominio/Paciente/IRepositorioPacientes.ts
@@ -1,12 +1,21 @@
 import { IPaciente } from './IPaciente.js';
 
 export interface IRepositorioPacientes {
+  existePacientePorDocumento(
+    numeroDoc: string,
+    tipoDoc: number
+  ): Promise<boolean>;
+
   obtenerPacientes(limite?: number): Promise<IPaciente[]>;
+
   obtenerPacientePorId(numeroDoc: string): Promise<IPaciente>;
+
   crearPaciente(nuevoPaciente: IPaciente): Promise<string>;
+
   actualizarPaciente(
     numeroDoc: string,
     paciente: IPaciente
   ): Promise<IPaciente>;
+
   borrarPaciente(numeroDoc: string): Promise<void>;
 }
