d728befadc57c759152de80acc35a64f87b4bfe7
isrameve <israelmedive@gmail.com>
2025-11-21 16:34:09 -0500

    refactor: eliminacion de estados por desuso

--- src/core/infraestructura/esquemas/consultorioEsquema.ts
+++ src/core/infraestructura/esquemas/consultorioEsquema.ts
@@ -1,22 +1,15 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const CrearConsultorioEsquema = z.object({
   idConsultorio: z
     .string()
-    .nonempty("El ID del consultorio es obligatorio")
-    .min(1, "El ID debe tener al menos 1 carácter")
-    .max(5, "El ID no puede exceder 5 caracteres"),
+    .nonempty('El ID del consultorio es obligatorio')
+    .min(1, 'El ID debe tener al menos 1 carácter')
+    .max(5, 'El ID no puede exceder 5 caracteres'),
   ubicacion: z
     .string()
     .optional()
     .transform((valor) => valor ?? null),
-  estado: z
-    .number()
-    .int("El estado debe ser un número entero")
-    .refine(
-      (val) => val === 6 || val === 7,
-      { message: "El estado debe ser 6 (Ocupado) o 7 (Disponible)" }
-    ),
 });
 
 export type ConsultorioDTO = z.infer<typeof CrearConsultorioEsquema>;
