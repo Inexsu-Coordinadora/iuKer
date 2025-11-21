eebcf1df89ca57e7bb6f1f23a5aded5345d23227
isrameve <israelmedive@gmail.com>
2025-11-20 21:48:28 -0500

    style: cambiar una letra para cumplir convención de nombramientos

--- src/core/infraestructura/rutas/citasMedicasEnrutador.ts
+++ src/core/infraestructura/rutas/citasMedicasEnrutador.ts
@@ -7,26 +7,42 @@ import { AgendamientoCitasCasosUso } from '../../aplicacion/servicios/agendamien
 import { MedicosRepositorio } from '../repositorios/postgres/MedicosRepositorio.js';
 import { PacientesRepositorio } from '../repositorios/postgres/PacientesRepositorio.js';
 
-function citasMedicasEnrutador(app: FastifyInstance, citasMedicasController: CitasMedicasControlador) {
+function citasMedicasEnrutador(
+  app: FastifyInstance,
+  citasMedicasController: CitasMedicasControlador
+) {
   app.get('/citas-medicas', citasMedicasController.obtenerCitas);
   app.get('/citas-medicas/:idCita', citasMedicasController.obtenerCitaPorId);
   app.post('/citas-medicas', citasMedicasController.agendarCita);
-  app.put('/citas-medicas/reprogramacion/:idCita', citasMedicasController.reprogramarCita);
-  app.put('/citas-medicas/finalizacion/:idCita', citasMedicasController.finalizarCita);
-  app.put('/citas-medicas/cancelacion/:idCita', citasMedicasController.cancelarCita);
-  app.delete('/citas-medicas/eliminacion/:idCita', citasMedicasController.eliminarCita);
+  app.put(
+    '/citas-medicas/reprogramacion/:idCita',
+    citasMedicasController.reprogramarCita
+  );
+  app.put(
+    '/citas-medicas/finalizacion/:idCita',
+    citasMedicasController.finalizarCita
+  );
+  app.put(
+    '/citas-medicas/cancelacion/:idCita',
+    citasMedicasController.cancelarCita
+  );
+  app.delete(
+    '/citas-medicas/eliminacion/:idCita',
+    citasMedicasController.eliminarCita
+  );
 }
 
-export async function construirCitasEnrutados(app: FastifyInstance) {
+export async function construirCitasEnrutador(app: FastifyInstance) {
   const citasMedicasRepositorio = new CitasMedicasRepositorio();
   const citasCasosUso = new CitasMedicasCasosUso(citasMedicasRepositorio);
 
   const medicoRepositorio = new MedicosRepositorio();
   const pacientesRepositorio = new PacientesRepositorio();
-  const cancelacionReprogramacionCasosUso = new CancelacionReprogramacionCitasCasosUso(
-    citasMedicasRepositorio,
-    medicoRepositorio
-  );
+  const cancelacionReprogramacionCasosUso =
+    new CancelacionReprogramacionCitasCasosUso(
+      citasMedicasRepositorio,
+      medicoRepositorio
+    );
 
   const agendamientoCitaCasosUso = new AgendamientoCitasCasosUso(
     citasMedicasRepositorio,
